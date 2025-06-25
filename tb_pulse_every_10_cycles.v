da9d6e14ce551c7136641f36d1555f646b39496e
K SREE SAI VENKAT <2003ssv@gmail.com>
2025-06-25 17:27:17 +0530

    Update tb_pulse_every_10_cycles.v

--- tb_pulse_every_10_cycles.v
+++ tb_pulse_every_10_cycles.v
@@ -1,28 +1,29 @@
 `timescale 1ns / 1ps
 
-module op_sig_counter_tb;
+module tb_pulse_every_10_cycles ;
 
    reg clk;
-initial clk = 0;
-
-    reg start = 0;
-    reg rst = 0;
+   reg start = 0;
+   reg rst = 0;
     
-    wire op_sig;
+   wire op_sig;
 
-    op_sig_counter uut (.clk(clk), .start(start), .rst(rst), .op_sig(op_sig) );
+   initial clk = 0;
+
+    pulse_every_10_cycles uut (.clk(clk), .start(start), .rst(rst), .op_sig(op_sig) );
 
     always #5 clk = ~clk; 
 
-    initial begin  
-        $display("Time\t clk\t start\t rst\t op_sig");
-        $monitor("%0t\t %b\t %b\t %b\t %b", $time, clk, start, rst, op_sig);
-
-        #10 rst = 1;
-        #10 rst = 0; start = 1;
-        #100 start = 0; 
-        #20 $finish;
-    end
+    initial 
+       begin  
+        #10 
+          rst = 1;
+        #10 
+          rst = 0; start = 1;
+        #100 
+          start = 0; 
+        #20 
+          $finish;
+       end
    
 endmodule
-
